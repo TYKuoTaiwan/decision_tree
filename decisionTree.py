c0bb4977c52993fea4ae375bb97663459a03fa54
Tzu-Yi Kuo <kuoze1991@gmail.com>
2017-02-20 16:55:02 -0600

    In this revision, the function for pruning decision tree was accomplished. The remaining work is to: 	- check how to use validation data 	- complete the output function 	- revise the hard coded input to user input 	- take down the bonus points

--- decisionTree.py
+++ decisionTree.py
@@ -3,6 +3,7 @@ import math
 import csv
 import sys
 import copy 
+import random
 
 class Node:
     def __init__(self, val):
@@ -13,21 +14,7 @@ class Node:
         self.v = val
         self.data=None
         self.flag =None
-
-    def getLeft(self):
-    	return self.l;
-
-    def getRight(self):
-    	return self.r;
-
-    def setVal(self, data):
-    	self.v = data;
-
-    def setClas(self, data):
-    	self.cla = data;
-
-    def setInter(self,data):
-    	self.inter = data;
+        self.alive = True
 
     def print_tree(self):
     	if self.inter:
@@ -38,6 +25,52 @@ class Node:
     		self.l.print_tree()
     	if self.r:
             self.r.print_tree()
+
+    def __nodeCount(self,counter):#nodeCount
+    	if self:
+    		counter[0] += 1;
+    	if self.l:
+    		self.l.__nodeCount(counter)
+    	if self.r:
+            self.r.__nodeCount(counter)
+
+    def nodeCount(self):
+    	counter =np.zeros(1);
+    	self.__nodeCount(counter);
+    	return counter[0].astype('int');
+    
+    def __leaveCount(self, counter):
+    	if self.inter == False:
+    		counter[0] += 1;
+    	if self.l:
+    		self.l.__leaveCount(counter)
+    	if self.r:
+            self.r.__leaveCount(counter)
+
+    def leaveCount(self):
+    	counter =np.zeros(1);
+    	self.__leaveCount(counter);
+    	return counter[0].astype('int');
+
+    def killNode(self,idx,step):
+    	if self.inter:
+    		if idx == step[0]:
+    			self.alive = False;
+    			step[0] += 1;
+    		else:
+    			step[0] += 1;
+    			self.l.killNode(idx,step);
+    			self.r.killNode(idx,step);
+    def update(self):
+    	if self.alive == False:
+    		self.inter = False;
+    		self.cla = mainClass(self.data);
+    		self.l = None;
+    		self.r = None;
+    	if self.l:
+    		self.l.update()
+    	if self.r:
+            self.r.update()
 
 def entropy(dataVec):
 	numInstance = dataVec.shape[0];
@@ -141,7 +174,7 @@ def builtDecisionTree(data,flag,node):
 		node.cla = mainClass(data);
 		return;
 
-	node.setVal(decAttr);
+	node.v = decAttr;
 	node.data = data;
 	node.r = Node(-1);
 	node.l = Node(-2);
@@ -191,15 +224,24 @@ def pruneTree(root, pruneFactor,attrNum):
 	pruneNode = int(pruneFactor*attrNum);
 	arr = np.array([]);
 	node = root;
-
-
+	nodeNum = root.nodeCount();
+	leaveNum = root.leaveCount();
+	interNodeNum = nodeNum-leaveNum;
+	pruneNodeIdx=random.sample(range(interNodeNum), pruneNode);
+	for idx in range(pruneNode):
+		global step;
+		step=np.zeros(1);
+		root.killNode(pruneNodeIdx[idx],step);
+	root.update();
 
 # read data
 fileTraining = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/training_set.csv";
 fileValidate = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/validation_set.csv";
 fileTest = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/test_set.csv";
+pruneFactor = 0.2;
 # fileTraining=sys.argv[1];
 # fileTest=sys.argv[2];
+# pruneFactor = sys.argv[3];
 
 	# read training data
 raw_data = open(fileTraining)
@@ -208,51 +250,50 @@ Temp = np.array(list(reader))
 header = Temp[0,];
 dataTraining = Temp[1:,].astype('bool_');
 
-	# read test data
-# raw_data = open(fileValidate)
-# reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE);
-# Temp = np.array(list(reader))
-# dataValidation = Temp[1:,].astype('bool_');
-
 	# read validation data
+raw_data = open(fileValidate)
+reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE);
+Temp = np.array(list(reader))
+dataValidation = Temp[1:,].astype('bool_');
+
+	# read test data
 raw_data = open(fileTest)
 reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE);
 Temp = np.array(list(reader))
 dataTest = Temp[1:,].astype('bool_');
 
+# setup basic parameters
 numAttr = header.shape[0]-1;
 numTrain = dataTraining.shape[0];
 numTest = dataTest.shape[0];
-# numValidation = dataValidation.shape[0];
 numTest = dataTest.shape[0];
 flag = np.ones(numAttr, dtype=bool);
 
-
-
-
 # training
-root = Node(-1)
+root = Node(-1);
 builtDecisionTree(dataTraining,flag,root);
+nodeNum = root.nodeCount();
+leaveNum = root.leaveCount();
 acTrain = useDecisionTree(dataTraining,root);
 
 # pruning 
 
 # testing
 acTest = useDecisionTree(dataTest,root);
+pruneTree(root,pruneFactor,numAttr);
+acTest2 = useDecisionTree(dataTest,root);
 
 print ("Number of training instances = ",numTrain)
 print ("Number of training attributes=", numAttr)
-# print ("Total number of nodes in the tree = 20")
-# print ("Number of leaf nodes in the tree = 8")
+print ("Total number of nodes in the tree =", nodeNum)
+print ("Number of leaf nodes in the tree =",leaveNum)
 print ("Accuracy of the model on the training dataset = ", acTrain)
 
 print ();
 
 print ("Number of test instances = ",numTest)
 print ("Number of test attributes=", numAttr)
-# print ("Total number of nodes in the tree = 20")
-# print ("Number of leaf nodes in the tree = 8")
+print ("Total number of nodes in the tree =", nodeNum)
+print ("Number of leaf nodes in the tree =", leaveNum)
 print ("Accuracy of the model on the test dataset = ", acTest)
-# print (flag);
-
-# print (b-a);
+print ("Accuracy 2 of the model on the test dataset = ", acTest2)
