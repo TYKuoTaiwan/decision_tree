e09239e3faf67458606acfc673c345de0daa3004
Tzu-Yi Kuo <kuoze1991@gmail.com>
2017-02-23 01:32:51 -0600

    The function for print the readable decision tree is done. The report will be generate automatically after the classification. The bonus part which is to construct a random decision tree is also done. The variable assiciated with the bonus part will end with "R", ex: rootR, flagR, etc. Input of the program is change to user-input mode. The progame will be excuted in Terminal by entering: python3 decisionTree.py [location of training data] [location of validation data] [location of test data] [prune factor (optional)]

--- decisionTree.py
+++ decisionTree.py
@@ -12,9 +12,11 @@ class Node:
         self.inter = True
         self.cla = False
         self.v = val
-        self.data=None
         self.flag =None
         self.alive = True
+        self.depth = 0
+        self.parent = None
+        self.successor = None
 
     def print_tree(self):
     	if self.inter:
@@ -51,6 +53,19 @@ class Node:
     	counter =np.zeros(1);
     	self.__leaveCount(counter);
     	return counter[0].astype('int');
+
+    def __avgDepth(self, depthArr):
+    	if self.inter == False:
+    		depthArr[0] += self.depth;
+    	if self.l:
+    		self.l.__avgDepth(depthArr)
+    	if self.r:
+            self.r.__avgDepth(depthArr)
+
+    def avgDepth(self):
+    	depthArr =np.zeros(1);
+    	self.__avgDepth(depthArr);
+    	return (depthArr[0]/self.leaveCount()).astype("float16");
 
     def killNode(self,idx,step):
     	if self.inter:
@@ -177,7 +192,13 @@ def builtDecisionTree(data,flag,node):
 	node.v = decAttr;
 	node.data = data;
 	node.r = Node(-1);
+	node.r.depth = node.depth+1;
+	node.r.parent = decAttr;
+	node.r.successor = "r"
 	node.l = Node(-2);
+	node.l.depth = node.depth+1;
+	node.l.parent = decAttr;
+	node.l.successor = "l"
 	flag[decAttr] = False;
 	dataTrue, dataFalse = seperateData(data,decAttr);
 	node.flag = flag;
@@ -191,14 +212,61 @@ def builtDecisionTree(data,flag,node):
 	else:
 		node.l.inter = False;
 		node.l.cla = mainClass(dataFalse);
-		# print (decAttr,dataFalse.shape[0], entropy(dataFalse[:,dataFalse.shape[1]-1]), dataFalse[:,dataFalse.shape[1]-1]);
 
 	if dataTrue.shape[0]!=0 and entropy(dataTrue[:,dataTrue.shape[1]-1]) != 0:
 		builtDecisionTree(dataTrue, flagTrue, node.r);
 	else:
 		node.r.inter = False;
 		node.r.cla = mainClass(dataTrue);
-		# print (decAttr,dataTrue.shape[0], entropy(dataTrue[:,dataTrue.shape[1]-1]), dataTrue[:,dataTrue.shape[1]-1]);
+
+def builtDecisionTreeRandom(data,flag,node):
+	decAttr = findRandom(flag);
+	if decAttr ==-1:
+		node.inter = False;
+		node.cla = mainClass(data);
+		return;
+
+	node.v = decAttr;
+	node.data = data;
+	node.r = Node(-1);
+	node.r.depth = node.depth+1;
+	node.r.parent = decAttr;
+	node.r.successor = "r"
+	node.l = Node(-2);
+	node.l.depth = node.depth+1;
+	node.l.parent = decAttr;
+	node.l.successor = "l"
+	flag[decAttr] = False;
+	dataTrue, dataFalse = seperateData(data,decAttr);
+	node.flag = flag;
+	flagFalse = copy.copy(flag);
+	flagTrue = copy.copy(flag);
+
+
+	if dataFalse.shape[0]!=0 and entropy(dataFalse[:,dataFalse.shape[1]-1]) != 0:
+		builtDecisionTreeRandom(dataFalse, flagFalse, node.l);
+	else:
+		node.l.inter = False;
+		node.l.cla = mainClass(dataFalse);
+
+	if dataTrue.shape[0]!=0 and entropy(dataTrue[:,dataTrue.shape[1]-1]) != 0:
+		builtDecisionTreeRandom(dataTrue, flagTrue, node.r);
+	else:
+		node.r.inter = False;
+		node.r.cla = mainClass(dataTrue);
+
+def findRandom(flag):
+	activeAttrNum = sum(flag);
+	if activeAttrNum == 0:
+		return -1;
+	selectedAttrOrder=random.sample(range(activeAttrNum), 1);
+	counter = 0;
+	for idx in range(flag.shape[0]):
+		if flag[idx]:
+			counter += 1;
+		if counter == selectedAttrOrder[0]:
+			break;
+	return idx;
 
 def useDecisionTree(data,root):
 	predict = np.zeros(data.shape[0]);
@@ -234,29 +302,50 @@ def pruneTree(root, pruneFactor,attrNum):
 		root.killNode(pruneNodeIdx[idx],step);
 	root.update();
 
-# read data
-fileTraining = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/training_set.csv";
-fileValidate = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/validation_set.csv";
-fileTest = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/test_set.csv";
-pruneFactor = 0.2;
-# fileTraining=sys.argv[1];
-# fileTest=sys.argv[2];
-# pruneFactor = sys.argv[3];
+def printTree(root,header):
+	if root.inter and root.parent:
+		indent = "";
+		for i in range(root.depth-1):
+			indent += "|";
+		if root.successor == "l":
+			print (indent,header[root.parent]," = 0 : ");
+		else:
+			print (indent,header[root.parent]," = 1 : ");
+	elif root.parent:
+		indent = "";
+		for i in range(root.depth-1):
+			indent += "|";
+		if root.successor == "l":
+			print (indent,header[root.parent]," = 0 :",int(root.cla));
+		else:
+			print (indent,header[root.parent]," = 1 :",int(root.cla));
+	if root.l:
+		printTree(root.l,header);
+	if root.r:
+		printTree(root.r,header);
 
-	# read training data
+# read data
+
+# fileTraining = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/training_set.csv";
+# fileValidate = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/validation_set.csv";
+# fileTest = "/Users/jarvisjr/Documents/17Spring/MachineLearning/assignment2/data_sets1/test_set.csv";
+# pruneFactor =0.2;
+fileTraining=sys.argv[1];
+fileValidate=sys.argv[2];
+fileTest=sys.argv[3];
+if len(sys.argv) == 5:
+	pruneFactor = float(sys.argv[4]);
+
+# read data
 raw_data = open(fileTraining)
 reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE);
 Temp = np.array(list(reader))
 header = Temp[0,];
 dataTraining = Temp[1:,].astype('bool_');
-
-	# read validation data
 raw_data = open(fileValidate)
 reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE);
 Temp = np.array(list(reader))
 dataValidation = Temp[1:,].astype('bool_');
-
-	# read test data
 raw_data = open(fileTest)
 reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE);
 Temp = np.array(list(reader))
@@ -265,35 +354,97 @@ dataTest = Temp[1:,].astype('bool_');
 # setup basic parameters
 numAttr = header.shape[0]-1;
 numTrain = dataTraining.shape[0];
+numValid = dataValidation.shape[0];
 numTest = dataTest.shape[0];
-numTest = dataTest.shape[0];
-flag = np.ones(numAttr, dtype=bool);
 
-# training
+flag = np.ones(numAttr, dtype=bool);
 root = Node(-1);
 builtDecisionTree(dataTraining,flag,root);
-nodeNum = root.nodeCount();
-leaveNum = root.leaveCount();
-acTrain = useDecisionTree(dataTraining,root);
 
-# pruning 
+flagR = np.ones(numAttr, dtype=bool);
+rootR = Node(-1);
+builtDecisionTreeRandom(dataTraining,flagR,rootR);
 
-# testing
-acTest = useDecisionTree(dataTest,root);
-pruneTree(root,pruneFactor,numAttr);
-acTest2 = useDecisionTree(dataTest,root);
+nodeNumR = rootR.nodeCount();
+leaveNumR = rootR.leaveCount();
 
+acTrainPreR = useDecisionTree(dataTraining,rootR);
+acValidPreR = useDecisionTree(dataValidation,rootR);
+acTestPreR = useDecisionTree(dataTest,rootR);
+
+# printTree(root,header);
+print ("Random-generated Accuracy");
+print ("-------------------------------------");
 print ("Number of training instances = ",numTrain)
 print ("Number of training attributes=", numAttr)
-print ("Total number of nodes in the tree =", nodeNum)
-print ("Number of leaf nodes in the tree =",leaveNum)
-print ("Accuracy of the model on the training dataset = ", acTrain)
+print ("Total number of nodes in the tree =", nodeNumR)
+print ("Number of leaf nodes in the tree =",leaveNumR)
+print ("Accuracy of the model on the training dataset = ", acTrainPreR)
+print ();
+
+print ("Number of validation instances = ",numValid)
+print ("Number of validation attributes=", numAttr)
+print ("Accuracy of the model on the validation dataset before pruning = ", acValidPreR)
 
 print ();
 
 print ("Number of test instances = ",numTest)
 print ("Number of test attributes=", numAttr)
-print ("Total number of nodes in the tree =", nodeNum)
-print ("Number of leaf nodes in the tree =", leaveNum)
-print ("Accuracy of the model on the test dataset = ", acTest)
-print ("Accuracy 2 of the model on the test dataset = ", acTest2)
+print ("Accuracy of the model on the test dataset before pruning = ", acTestPreR)
+
+print ();
+
+if len(sys.argv) == 4:
+	nodeNum = root.nodeCount();
+	leaveNum = root.leaveCount();
+
+	acTrainPre = useDecisionTree(dataTraining,root);
+	acValidPre = useDecisionTree(dataValidation,root);
+	acTestPre = useDecisionTree(dataTest,root);
+
+	print ("Pre-Pruned Accuracy");
+	print ("-------------------------------------");
+	print ("Number of training instances = ",numTrain)
+	print ("Number of training attributes=", numAttr)
+	print ("Total number of nodes in the tree =", nodeNum)
+	print ("Number of leaf nodes in the tree =",leaveNum)
+	print ("Accuracy of the model on the training dataset = ", acTrainPre)
+
+	print ();
+
+	print ("Number of validation instances = ",numValid)
+	print ("Number of validation attributes=", numAttr)
+	print ("Accuracy of the model on the validation dataset before pruning = ", acValidPre)
+
+	print ();
+
+	print ("Number of test instances = ",numTest)
+	print ("Number of test attributes=", numAttr)
+	print ("Accuracy of the model on the test dataset before pruning = ", acTestPre)
+
+else:
+	pruneTree(root,pruneFactor,numAttr);
+	nodeNum = root.nodeCount();
+	leaveNum = root.leaveCount();
+	acTrainPost = useDecisionTree(dataTraining,root);
+	acValidPost = useDecisionTree(dataValidation,root);
+	acTestPost = useDecisionTree(dataTest,root);
+	print ("Post-Pruned Accuracy");
+	print ("-------------------------------------");
+	print ("Number of training instances = ",numTrain)
+	print ("Number of training attributes=", numAttr)
+	print ("Total number of nodes in the tree =", nodeNum)
+	print ("Number of leaf nodes in the tree =",leaveNum)
+	print ("Accuracy of the model on the training dataset = ", acTrainPost)
+
+	print ();
+
+	print ("Number of validation instances = ",numValid)
+	print ("Number of validation attributes=", numAttr)
+	print ("Accuracy of the model on the validation dataset after pruning = ", acValidPost)
+
+	print ();
+
+	print ("Number of test instances = ",numTest)
+	print ("Number of test attributes=", numAttr)
+	print ("Accuracy of the model on the test dataset = ", acTestPost)
